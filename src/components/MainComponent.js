be4d7fdc2170faf9d8cbe2249e0a8c8b3c07367a
dwarkesh <bhayanidwarkesh@gmail.com>
2020-07-02 19:17:53 +0530

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -46,6 +47,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path="/contactus" component={Contact}/>
           <Route path='/menu/:dishId' component={DishWithId} />
