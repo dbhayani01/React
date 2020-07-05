f947c30c725bc98594fdd77f8e40b49ec299ae7b
dwarkesh <bhayanidwarkesh@gmail.com>
2020-07-05 20:57:21 +0530

    react animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -99,13 +100,19 @@ function RenderDish({dish}) {
         if(dish!=null) {
             const {name, image, description} = dish;
             return(
-                <Card>
-                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle><strong>{name}</strong></CardTitle>
-                        <CardText>{description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </FadeTransform>
             );    
         }
         else{
@@ -122,10 +129,14 @@ function RenderDish({dish}) {
                 date = convertDate(date);
                 console.log(comment);
                 return(
+                    <Stagger in>
+                    <Fade>
                     <div>
                         <p><li>{comment}</li></p>
                         <p><li>-- {author}, {date}</li></p>
-                    </div>           
+                    </div> 
+                    </Fade>  
+                    </Stagger>        
                 );
             });
             console.log(contents);
